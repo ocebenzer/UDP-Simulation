bb8872464dbad96c70123c0f166e7548a7f75605
ocb <ocebe@ocebe.ocebe>
2022-06-03 21:15:03 +0300

    looks good processwise

--- sdn/sdn.c
+++ sdn/sdn.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
 #include <unistd.h>
 #include <signal.h>
@@ -19,27 +18,91 @@ struct tunnel tunnels[TUNNELS_SIZE];
 char buffer[BUFFER_SIZE], token[TOKEN_SIZE];
 int tunnel_ctr, t_index;
 
-bool is_invalid_dest(char* dest) {
+int is_invalid_dest(char* dest) {
     u_int8_t ip[4];
     unsigned short port;
     int parse_status = sscanf(dest, "%hhu.%hhu.%hhu.%hhu:%hu", &ip[0], &ip[1], &ip[2], &ip[3], &port);
     return parse_status <= 0;
 }
 
+int create_tunnel(struct tunnel *t) {      
+    int fd[2]; // fd[1] -> fd[0]
+    if (pipe(fd) == -1) {
+        printf("Piping failed\n");
+        return 1;
+    }
+
+    // create sender
+    t->pid_sender = fork();
+    if (t->pid_sender < 0) {
+        printf("Couldn't create sender\n");
+        return 1;
+    }
+    else if (t->pid_sender == 0) {
+        close(fd[1]);
+        dup2(STDIN_FILENO, fd[0]);
+        sprintf(buffer, "udp-sendto:%s", t->dest);
+        printf("[%d] Creating Sender: \"%s\"\n", t_index, buffer);
+        int status = execl("/usr/bin/socat", "/usr/bin/socat", "-", buffer, NULL);
+        printf("[%d] Warning - Sender exit status %d\n", t_index, status);
+        exit(1);
+    }
+
+    // create listener
+    t->pid_listener = fork();
+    if (t->pid_listener < 0) {
+        printf("Couldn't create listener\n");
+        kill(t->pid_sender, SIGINT);
+        return 1;
+    }
+    else if (t->pid_listener == 0) {
+        close(fd[0]);
+        dup2(STDOUT_FILENO, fd[1]);
+        sprintf(buffer, "udp4-listen:%s", t->localport);
+        printf("[%d] Creating Listener: \"%s\"\n", t_index, buffer);
+        int status = execl("/usr/bin/socat", "/usr/bin/socat", "-", buffer, NULL);
+        printf("[%d] Warning - Listener exit status %d\n", t_index, status);
+        exit(1);
+    }
+
+    close(fd[0]);
+    close(fd[1]);
+    return 0;
+}
+
+int remove_tunnel(struct tunnel *t) {
+    int status_listener, status_sender;
+    if (t->pid_listener != 0){
+        status_listener = kill(t->pid_listener, SIGINT);
+        if (status_listener) {
+            printf("Error removing Listener PID %d\n", t->pid_listener);
+        }
+    }
+    if (t->pid_sender != 0) {
+        status_sender = kill(t->pid_sender, SIGINT);
+        if (status_sender) {
+            printf("Error removing Sender PID %d\n", t->pid_sender);
+        }
+    }
+    return (status_listener || status_sender) ? 1 : 0;
+}
+
 int main() {
     printf("SDN Started\n");
-    while(true) {
+    while(1) {
+        usleep(200000);
         memset(token, 0, sizeof(token));
+        printf("> ");
         fgets(buffer, BUFFER_SIZE, stdin);
         sscanf(buffer, "%s", token);
         if (!strcmp(token, "help")) {
             printf("Listing all commands:\n"
-                "help - this\n"
-                "list - lists all tunnels\n"
-                "add [LOCALPORT] [DEST_IP]:[DEST_PORT] - adds a tunnel, returns TUNNEL_ID\n"
-                "update [TUNNEL_ID] [DEST_IP]:[DEST_PORT] - updates a tunnel, returns TUNNEL_ID\n"
-                "remove [TUNNEL_ID] - removes a tunnel\n"
-                "exit - exit\n");
+                "- help - this\n"
+                "- list - lists all tunnels\n"
+                "- add [LOCALPORT] [DEST_IP]:[DEST_PORT] - adds a tunnel, returns TUNNEL_ID\n"
+                "- update [TUNNEL_ID] [DEST_IP]:[DEST_PORT] - updates a tunnel, returns TUNNEL_ID\n"
+                "- remove [TUNNEL_ID] - removes a tunnel\n"
+                "- exit - exit\n");
         }
         else if (!strcmp(token, "list")) {
             printf("Listing all tunnels:\n");
@@ -49,6 +112,7 @@ int main() {
                 }
             }
         }
+        else if (!strcmp(token, "")) continue;
         else if (!strcmp(token, "add")) {
             if (tunnel_ctr == TUNNELS_SIZE) {
                 printf("Maximum tunnel amount reacehd, please delete one\n");
@@ -58,25 +122,17 @@ int main() {
             int parse_status = sscanf(buffer, "%*s %s %s",
                 tunnels[t_index].localport, tunnels[t_index].dest);
             if (parse_status < 0 || is_invalid_dest(tunnels[t_index].dest)) {
-                printf("Couldn't parse %s", buffer);
+                printf("Couldn't parse %s\n", buffer);
                 continue;
             }
-            tunnels[t_index].pid_sender = fork();
-            if (tunnels[t_index].pid_sender < 0) {
-                printf("Couldn't create tunnel\n");
+
+            if (create_tunnel(&tunnels[t_index])) {
+                printf("Couldn't create %s\n", buffer);
                 continue;
             }
-            else if (tunnels[t_index].pid_sender == 0) {
-                sprintf(buffer, "/usr/bin/socat - udp4-listen:%s | /usr/bin/socat - udp-sendto:%s", tunnels[t_index].localport, tunnels[t_index].dest);
-                printf("Tunnel command: \"%s\"\n", buffer);
-                int status = execl("/bin/sh", "/bin/sh", "-c", buffer, NULL);
-                printf("Warning - Tunnel [%d] might not be added - execl status %d\n", t_index, status);
-                exit(1);
-            }
-            else {
-                tunnel_ctr++;
-                printf("[%d] added, PID %d -> %d\n", t_index, tunnels[t_index].pid_listener, tunnels[t_index].pid_sender);
-            }
+
+            tunnel_ctr++;
+            printf("[%d] added, PID %d -> %d\n", t_index, tunnels[t_index].pid_listener, tunnels[t_index].pid_sender);
         }
         else if (!strcmp(token, "update")) {
             int parse_status = sscanf(buffer, "%*s %d %s",&t_index, token);
@@ -85,61 +141,32 @@ int main() {
                 printf("Couldn't parse %s", buffer);
                 continue;
             }
-            if (kill(tunnels[t_index].pid_listener, SIGINT)) {
-                printf("Error removing old Tunnel [%d]\n", tunnels[t_index].pid_listener);
+
+            remove_tunnel(&tunnels[t_index]);
+
+            if (create_tunnel(&tunnels[t_index])) {
+                printf("Couldn't create %s\n", buffer);
                 continue;
             }
-            if (kill(tunnels[t_index].pid_sender, SIGINT)) {
-                printf("Error removing old Tunnel [%d]\n", tunnels[t_index].pid_sender);
-                continue;
-            }
-            tunnels[t_index].pid_sender = fork();
-            if (tunnels[t_index].pid_sender < 0) {
-                printf("Couldn't create tunnel\n");
-                continue;
-            }
-            else if (tunnels[t_index].pid_sender == 0) {
-                sprintf(buffer, "/usr/bin/socat - udp4-listen:%s | /usr/bin/socat - udp-sendto:%s", tunnels[t_index].localport, tunnels[t_index].dest);
-                printf("Tunnel command: \"%s\"\n", buffer);
-                int status = execl("/bin/sh", "/bin/sh", "-c", buffer, NULL);
-                printf("Warning - Tunnel [%d] might not be added - execl status %d\n", t_index, status);
-                exit(1);
-            }
-            else {
-                printf("[%d] updated, PID %d -> %d\n", t_index, tunnels[t_index].pid_listener, tunnels[t_index].pid_sender);
-            }
+
+            printf("[%d] updated, PID %d -> %d\n", t_index, tunnels[t_index].pid_listener, tunnels[t_index].pid_sender);
         }
         else if (!strcmp(token, "remove")) {
             sscanf(buffer, "%*s %d",&t_index);
-            int pid_listener = tunnels[t_index].pid_listener;
-            int pid_sender = tunnels[t_index].pid_sender;
+
+            remove_tunnel(&tunnels[t_index]);
+
             memset(&tunnels[t_index], 0, sizeof(struct tunnel));
-            if (pid_sender == 0 || pid_listener) {
-                printf("Error, %d is not a valid tunnel\n", t_index);
-                continue;
-            }
-            if (kill(pid_listener, SIGINT)) {
-                printf("Error removing Tunnel [%d]\n", pid_listener);
-                continue;
-            }
-            if (kill(pid_sender, SIGINT)) {
-                printf("Error removing Tunnel [%d]\n", pid_sender);
-                continue;
-            }
             tunnel_ctr--;
             printf("[%d] removed\n", t_index);
         }
         else if (!strcmp(token, "exit")) {
             printf("Removing tunnels..");
             for (int i = 0; i < TOKEN_SIZE; i++) {
-                if (tunnels[i].pid_sender) {
-                    kill(tunnels[i].pid_listener, SIGINT);
-                    kill(tunnels[i].pid_sender, SIGINT);
-                }
+                remove_tunnel(&tunnels[i]);
             }
             break;
         }
-        else if (!strcmp(token, "")) continue;
         else {
             printf("invalid token %s, type help\n", token);
         }
